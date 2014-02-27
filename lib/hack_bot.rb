77873b844c187f024d7e8c4c67030a2cb4b1b1f7
Geoff Petrie <g.petrie@gmail.com>
2014-02-26 23:48:11 -0700

    Remove commented code

--- lib/hack_bot.rb
+++ lib/hack_bot.rb
@@ -3,13 +3,6 @@ require 'cinch'
 require 'hack_bot/channel_interaction'
 
 module HackBot
-
-  #def self.next_event(time, date, location)
-    #EVENT_TIME = time
-    #EVENT_DATE = date
-    #EVENT_LOCATION = location
-  #end
-
   def self.start(server, channels, nick, realname, user)
     bot = Cinch::Bot.new do
       configure do |c|
