1a239d93f5c10b9322bc07fa14a7b2ee911faa5a
Geoff Petrie <g.petrie@gmail.com>
2014-02-27 21:38:46 -0700

    Update rake default task add pending specs
    I believe this should allow for travis to build this gem. I need to work
    out how to abstract the Cinch API from the tests. I have a couple of
    leads, but for the moment these will need to be pending specs.

---
+++ spec/hack_bot/channel_interaction_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe ChannelInteraction do
+  it 'prints information about the next meeting' do
+    pending
+  end
+
+  it 'replies to a lone hello' do
+    pending
+  end
+
+  it 'likes nachos like everyone else does' do
+    pending
+  end
+
+  it 'will inform you of a who request' do
+    pending
+  end
+end
